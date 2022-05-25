1a7d7dba21cedd26809a67db0a0e0ed7f7194276
Dario Cangialosi Arkenidar (DCA) <arkenidar@users.noreply.github.com>
2022-05-25 18:28:15 +0000

    void rimuovi(nodo quale) + nodo prendi(int indice)

--- main.c
+++ main.c
@@ -1,40 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-    struct Nodo{
-        int contenuto; // lista di "int"
-        struct Nodo* collegato; // nodo successivo
+struct Nodo{
+    int contenuto; // lista di "int"
+    struct Nodo* successivo; // nodo successivo
 /*
 liste doppiamente collegate (nodo precedente oltre quello successivo), per scorrimento inverso e operazioni di rimozione e non solo
 */
-        struct Nodo* precedente;
-    };
-    struct Nodo* lista=NULL; // collegato al primo nodo
+    struct Nodo* precedente;
+};
+typedef struct Nodo* nodo;
 
-    struct Nodo* ultimo=NULL;
+nodo primo=NULL; // collegato al primo nodo
+
+nodo ultimo=NULL;
+
+void rimuovi(nodo quale){
+    if(!quale) return; // puntatore valido
+
+    // primo
+    if(quale==primo) primo=quale->successivo;
+
+    // ultimo (può essere sia primo che ultimo simultaneamente)
+    if(quale==ultimo) ultimo=quale->precedente;
+
+    // precedente
+    if(quale->precedente)
+    quale->precedente->successivo=quale->successivo;
+
+    // successivo
+    if(quale->successivo)
+    quale->successivo->precedente=quale->precedente;
+
+    free(quale); // de-alloca (quel che è stato allocato con malloc)
+}
+
+nodo prendi(int indice){
+    //printf("Scorri dal primo all'ultimo\n");
+    struct Nodo* scorre=primo;
+    int i=0;
+    while(scorre != NULL){
+
+        if(i==indice)
+        return scorre;
+
+        //printf("scorrendo n=%d\n",scorre->contenuto);
+        scorre=scorre->successivo;
+
+        i++;
+    }
+    return NULL;
+}
+
+int main(){
+
     for(int n=1; n<=3; n++){ // aggiungi: quanti?
 
         printf("aggiungi n=%d\n",n);
-        
-        struct Nodo* corrente=(struct Nodo*)malloc(sizeof (struct Nodo));
+
+        struct Nodo* corrente=(struct Nodo*)malloc(sizeof (struct Nodo)); // alloca
         corrente->contenuto=n;
-        corrente->collegato=NULL;
+        corrente->successivo=NULL;
 
         corrente->precedente=ultimo;
-        
-        if(ultimo==NULL) lista=corrente;
-        else ultimo->collegato=corrente;
+
+        if(ultimo==NULL) primo=corrente;
+        else ultimo->successivo=corrente;
 
         ultimo=corrente;
     }
 
+    nodo scorre;
+
     printf("Scorri dal primo all'ultimo\n");
-    struct Nodo* scorre=lista;
+    scorre=primo;
     while(scorre != NULL){
         printf("scorrendo n=%d\n",scorre->contenuto);
-        scorre=scorre->collegato;
+        scorre=scorre->successivo;
     }
 
     printf("Ordine inverso\n");
@@ -42,6 +84,15 @@ liste doppiamente collegate (nodo precedente oltre quello successivo), per scorr
     while(scorre != NULL){
         printf("scorrendo n=%d\n",scorre->contenuto);
         scorre=scorre->precedente;
+    }
+
+    rimuovi(prendi(2)); // parte da indice=0 come primo
+
+    printf("Scorri dal primo all'ultimo\n");
+    scorre=primo;
+    while(scorre != NULL){
+        printf("scorrendo n=%d\n",scorre->contenuto);
+        scorre=scorre->successivo;
     }
 
     return 0;
