af55827f3dae0c7bb5a50e4f2dbf3f2707beca9e
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2025-12-25 00:15:04 +0100

    [code-format] Refactor main.c for improved readability and consistency in code formatting

--- main.c
+++ main.c
@@ -1,98 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Nodo{
-    int contenuto; // lista di "int"
-    struct Nodo* successivo; // nodo successivo
 /*
-liste doppiamente collegate (nodo precedente oltre quello successivo), per scorrimento inverso e operazioni di rimozione e non solo
+liste doppiamente collegate
+(nodo precedente oltre quello successivo),
+ per scorrimento inverso e operazioni di rimozione e non solo
 */
-    struct Nodo* precedente;
+struct Nodo
+{
+    int contenuto;           // lista di "int"
+    struct Nodo *successivo; // nodo successivo
+    struct Nodo *precedente;
 };
-typedef struct Nodo* nodo;
+typedef struct Nodo *nodo;
 
-nodo primo=NULL; // collegato al primo nodo
+nodo primo = NULL; // collegato al primo nodo
 
-nodo ultimo=NULL;
+nodo ultimo = NULL;
 
-void rimuovi(nodo quale){
-    if(!quale) return; // puntatore valido
+void rimuovi(nodo quale)
+{
+    if (!quale)
+        return; // puntatore valido
 
     // primo
-    if(quale==primo) primo=quale->successivo;
+    if (quale == primo)
+        primo = quale->successivo;
 
     // ultimo (può essere sia primo che ultimo simultaneamente)
-    if(quale==ultimo) ultimo=quale->precedente;
+    if (quale == ultimo)
+        ultimo = quale->precedente;
 
     // precedente
-    if(quale->precedente)
-    quale->precedente->successivo=quale->successivo;
+    if (quale->precedente)
+        quale->precedente->successivo = quale->successivo;
 
     // successivo
-    if(quale->successivo)
-    quale->successivo->precedente=quale->precedente;
+    if (quale->successivo)
+        quale->successivo->precedente = quale->precedente;
 
     free(quale); // de-alloca (quel che è stato allocato con malloc)
 }
 
-nodo prendi(int indice){
-    //printf("Scorri dal primo all'ultimo\n");
-    struct Nodo* scorre=primo;
-    int i=0;
-    while(scorre != NULL){
+nodo prendi(int indice)
+{
+    // printf("Scorri dal primo all'ultimo\n");
+    struct Nodo *scorre = primo;
+    int i = 0;
+    while (scorre != NULL)
+    {
 
-        if(i==indice)
-        return scorre;
+        if (i == indice)
+            return scorre;
 
-        //printf("scorrendo n=%d\n",scorre->contenuto);
-        scorre=scorre->successivo;
+        // printf("scorrendo n=%d\n",scorre->contenuto);
+        scorre = scorre->successivo;
 
         i++;
     }
     return NULL;
 }
 
-int main(){
+int main()
+{
 
-    for(int n=1; n<=3; n++){ // aggiungi: quanti?
+    for (int n = 1; n <= 3; n++)
+    { // aggiungi: quanti?
 
-        printf("aggiungi n=%d\n",n);
+        printf("aggiungi n=%d\n", n);
 
-        struct Nodo* corrente=(struct Nodo*)malloc(sizeof (struct Nodo)); // alloca
-        corrente->contenuto=n;
-        corrente->successivo=NULL;
+        struct Nodo *corrente = (struct Nodo *)malloc(sizeof(struct Nodo)); // alloca
+        corrente->contenuto = n;
+        corrente->successivo = NULL;
 
-        corrente->precedente=ultimo;
+        corrente->precedente = ultimo;
 
-        if(ultimo==NULL) primo=corrente;
-        else ultimo->successivo=corrente;
+        if (ultimo == NULL)
+            primo = corrente;
+        else
+            ultimo->successivo = corrente;
 
-        ultimo=corrente;
+        ultimo = corrente;
     }
 
     nodo scorre;
 
     printf("Scorri dal primo all'ultimo\n");
-    scorre=primo;
-    while(scorre != NULL){
-        printf("scorrendo n=%d\n",scorre->contenuto);
-        scorre=scorre->successivo;
+    scorre = primo;
+    while (scorre != NULL)
+    {
+        printf("scorrendo n=%d\n", scorre->contenuto);
+        scorre = scorre->successivo;
     }
 
     printf("Ordine inverso\n");
-    scorre=ultimo;
-    while(scorre != NULL){
-        printf("scorrendo n=%d\n",scorre->contenuto);
-        scorre=scorre->precedente;
+    scorre = ultimo;
+    while (scorre != NULL)
+    {
+        printf("scorrendo n=%d\n", scorre->contenuto);
+        scorre = scorre->precedente;
     }
 
     rimuovi(prendi(2)); // parte da indice=0 come primo
 
     printf("Scorri dal primo all'ultimo\n");
-    scorre=primo;
-    while(scorre != NULL){
-        printf("scorrendo n=%d\n",scorre->contenuto);
-        scorre=scorre->successivo;
+    scorre = primo;
+    while (scorre != NULL)
+    {
+        printf("scorrendo n=%d\n", scorre->contenuto);
+        scorre = scorre->successivo;
     }
 
     return 0;
